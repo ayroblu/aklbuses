c26fa121f9d31a8b05e2fe4dbbfd364eb512fd76
Ben Lu <ben@ben-l.com>
2016-08-26 09:22:07 +1200

    Got it all working

---
+++ scripts/markerHelper.js
@@ -0,0 +1,107 @@
+import mapsModule from 'google-maps-api'
+
+module.exports = {
+  drawMarker(dataPart, map){
+    var p = dataPart;
+    var cc = [new google.maps.LatLng(p.ll.lat, p.ll.lng)];
+
+    var marker = new google.maps.Marker({
+      position: p.ll
+    , map: map
+    , title: p.title
+    , icon: {
+        path: google.maps.SymbolPath.CIRCLE
+      , scale: 6
+      , strokeWeight: 2
+      },
+    });
+
+    var infoWindow = this.drawInfoWindow(dataPart, map, cc)
+
+    this.markerShowPopup(dataPart, map, marker, infoWindow);
+
+    return marker;
+  }
+, markerShowPopup(dataPart, map, marker, infoWindow){
+    google.maps.event.addListener(marker, 'mouseover', function (event) {
+      // Within the event listener, "this" refers to the polygon which
+      // received the event.
+      this.setOptions({
+        strokeColor: '#00ff00',
+        fillColor: '#00ff00'
+      });
+
+      infoWindow.open(map);
+
+      var iwOuter = document.querySelector('.gm-style-iw');
+
+      var iwBackground = iwOuter.previousElementSibling;
+
+      // Remove the background shadow DIV
+      for (var i = 0; i < 4; ++i) {
+        Object.assign(iwBackground.children[i].style, {
+          display: 'none'
+        })
+      }
+      Object.assign(iwOuter.parentElement.parentElement.style, {
+        pointerEvents: 'none'
+      })
+      var iwCloseBtn = iwOuter.nextElementSibling;
+
+      Object.assign(iwCloseBtn.style, {
+        display: 'none'
+      })
+    });
+    // STEP 5: Listen for when the mouse stops hovering over the polygon.
+    google.maps.event.addListener(marker, 'mouseout', function (event) {
+      this.setOptions({
+        //strokeColor: '#ff0000',
+        //fillColor: getColour(dataPart.val)
+      });
+      infoWindow.close(map);
+    });
+  }
+, drawInfoWindow(dataPart, map, cc){
+    var featureData = dataPart
+    var dataText = `
+      <div class="popupData">
+        <h2>${featureData.name}</h2>
+        <p>${featureData.longName}</p>
+        <p>UTC: ${featureData.datetime.toISOString()}</p>
+        <p>Started at: ${featureData.startTime}</p>
+      </div>
+    `
+    var infoWindow = new google.maps.InfoWindow({
+      content: dataText
+    });
+    var bounds = new google.maps.LatLngBounds();
+    cc.forEach(c=>bounds.extend(c))
+    infoWindow.setPosition(bounds.getCenter());
+    return infoWindow
+  }
+, animateMarker(marker, oldPos, newPos){
+    // store a LatLng for each step of the animation
+    var frames = [];
+    for (var percent = 0; percent < 1; percent += 0.01) {
+      var pos = {
+        lat: oldPos.lat + percent * (newPos.lat - oldPos.lat)
+      , lng: oldPos.lng + percent * (newPos.lng - oldPos.lng)
+      }
+      frames.push(new google.maps.LatLng(pos.lat, pos.lng));
+    }
+
+    var move = function(marker, latlngs, index, wait) {
+      if (!latlngs[index]){
+        return;
+      }
+      marker.setPosition(latlngs[index]);
+      setTimeout(function() { 
+        move(marker, latlngs, index+1, wait); 
+      }, wait);
+    }
+
+    // begin animation, send back to origin after completion
+    move(marker, frames, 0, 20, marker.position);
+  }
+}
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,16 @@
-// This example uses the Google Maps JavaScript API's Data layer
-// to create a rectangular polygon with 2 holes in it.
-
-//var controls = document.querySelector('.controls')
-//var sControls;
 import './main'
 import {qget, qgeta} from './helper'
 import socketData from './socketdata'
-//require('./main')
-//var helper = require('./helper')
-//var socketData = require('./socketdata')
 import mapsModule from 'google-maps-api'
 const mapsapi = mapsModule('AIzaSyBkSlPom4tp0ypqQpZela8ct4VIjh2OoN8');
-//script(src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkSlPom4tp0ypqQpZela8ct4VIjh2OoN8&callback=initMap")
+import markerHelper from './markerHelper'
  
 mapsapi().then( function( maps ) {
   initMap()
   //use the google.maps object as you please 
 });
 
-window.store = {data: [], markers: {}, posData: {}};
-//var states = [false, false]
+window.store = {data: [], markers: {}};
 var map;
 
 function initMap() {
@@ -30,157 +21,82 @@ function initMap() {
 
   socketData.startRefreshing(null) //put in map
   socketData.setChatListener(function(d){
-    showData(d)
+    liveData.showData(d)
   })
 }
-function showData(data){
-  console.log('Showing data')
+var liveData = {
+  showData(data){
+    console.log('Showing data')
 
-  var positions = data.entity.map(e=>{
-    var pos = e.vehicle.position
-    var ll = {lat: pos.latitude, lng: pos.longitude}
-    var routeId = e.vehicle.trip.route_id
-    return {
-      ll: ll
-    , id: e.id
-    , vehicle_id: e.vehicle.vehicle.id
-    , name: data.routes[routeId].route_short_name
-    , longName: data.routes[routeId].route_long_name
-    , datetime: new Date(e.vehicle.timestamp*1000)
-    , startTime: e.vehicle.trip.start_time
-    }
-  })
-  store.data = positions
-  updateMarkers(positions, map)
-}
-function updateMarkers(data, map){
-  //okay, here's what we got
-  // new data object, looking at all the entities, check their ids
-  // Build three object stacks, same, removed, added
-  // same -> update marker position (stop using markers...)
-  // removed -> remove a marker
-  // added -> add a marker -> can basically call showData
-  var getGroupedData = function(data){
-    var keys = Object.keys(store.markers)
-    var groupedData = data.reduce((o, d)=>{
-      if (keys.indexOf(d.vehicle_id) >= 0){
-        o.moveData.push(d)
-        keys.splice(keys.indexOf(d.vehicle_id), 1)
-      } else {
-        o.addedData.push(d)
+    var positions = data.entity.map(e=>{
+      var pos = e.vehicle.position
+      var ll = {lat: pos.latitude, lng: pos.longitude}
+      var routeId = e.vehicle.trip.route_id
+      return {
+        ll: ll
+      , id: e.id
+      , vehicle_id: e.vehicle.vehicle.id
+      , name: data.routes[routeId].route_short_name
+      , longName: data.routes[routeId].route_long_name
+      , datetime: new Date(e.vehicle.timestamp*1000)
+      , startTime: e.vehicle.trip.start_time
       }
-      return o
-    }, {moveData: [], removedData: [], addedData: []})
-    groupedData.removedData = keys.map(k=>data[k])
-    return groupedData
-  }
-  //var groupedData = {moveData: [], removedData: [], addedData: []}
-  var groupedData = getGroupedData(data)
-
-  var moveMarkers = function(data){
-    // get markers by data, re
-    var markers = store.markers
-    data.forEach(d=>{
-      markers[d.vehicle_id].setPosition(new google.maps.LatLng(d.ll.lat, d.ll.lng))
     })
+    store.data = positions
+    this.updateMarkers(positions, map)
   }
-  var removeMarkers = function(data){
-    var markers = store.markers
-    data.forEach(d=>{
-      markers[d.vehicle_id].setMap(null)
-      markers[d.vehicle_id] = undefined
-      delete markers[d.vehicle_id]
-    })
-    // data.map(d=>d.remove)
-  }
-  var addMarkers = function(data){
-    data.forEach(d=>{
-      store.markers[d.vehicle_id] = drawMarker(d, map)
-    })
-  }
-  moveMarkers(groupedData.moveData)
-  removeMarkers(groupedData.moveData)
-  addMarkers(groupedData.addedData)
-}
-function drawMarker(dataPart, map){
-  var p = dataPart;
-  var cc = [new google.maps.LatLng(p.ll.lat, p.ll.lng)];
+, updateMarkers(data, map){
+    //okay, here's what we got
+    // new data object, looking at all the entities, check their ids
+    // Build three object stacks, same, removed, added
+    // same -> update marker position (stop using markers...)
+    // removed -> remove a marker
+    // added -> add a marker -> can basically call showData
+    var getGroupedData = function(data){
+      var keys = Object.keys(store.markers)
+      var groupedData = data.reduce((o, d)=>{
+        if (keys.indexOf(d.vehicle_id) >= 0){
+          o.moveData.push(d)
+          keys.splice(keys.indexOf(d.vehicle_id), 1)
+        } else {
+          o.addedData.push(d)
+        }
+        return o
+      }, {moveData: [], removedData: [], addedData: []})
+      groupedData.removedData = data.filter(d=>keys.indexOf(d.vehicle_id) >= 0) //keys.map(k=>data[k])
+      return groupedData
+    }
+    var groupedData = getGroupedData(data)
+    console.log(groupedData)
 
-  var marker = new google.maps.Marker({
-    position: p.ll
-  , map: map
-  , title: p.title
-  , icon: {
-      path: google.maps.SymbolPath.CIRCLE
-    , scale: 6
-    , strokeWeight: 2
-    },
-  });
-
-  var infoWindow = drawInfoWindow(dataPart, map, cc)
-
-  markerShowPopup(dataPart, map, marker, infoWindow);
-
-  return marker;
-}
-function markerShowPopup(dataPart, map, marker, infoWindow){
-  google.maps.event.addListener(marker, 'mouseover', function (event) {
-    // Within the event listener, "this" refers to the polygon which
-    // received the event.
-    this.setOptions({
-      strokeColor: '#00ff00',
-      fillColor: '#00ff00'
-    });
-
-    infoWindow.open(map);
-
-    var iwOuter = document.querySelector('.gm-style-iw');
-
-    var iwBackground = iwOuter.previousElementSibling;
-
-    // Remove the background shadow DIV
-    for (var i = 0; i < 4; ++i) {
-      Object.assign(iwBackground.children[i].style, {
-        display: 'none'
+    var moveMarkers = function(data){
+      // get markers by data, re
+      var markers = store.markers
+      data.forEach(d=>{
+        //markers[d.vehicle_id].setPosition(new google.maps.LatLng(d.ll.lat, d.ll.lng))
+        var marker = markers[d.vehicle_id]
+        markerHelper.animateMarker(markers[d.vehicle_id]
+        , {lat: marker.getPosition().lat(), lng: marker.getPosition().lng()}
+        , {lat: d.ll.lat, lng: d.ll.lng})
       })
     }
-    Object.assign(iwOuter.parentElement.parentElement.style, {
-      pointerEvents: 'none'
-    })
-    var iwCloseBtn = iwOuter.nextElementSibling;
-
-    Object.assign(iwCloseBtn.style, {
-      display: 'none'
-    })
-  });
-  // STEP 5: Listen for when the mouse stops hovering over the polygon.
-  google.maps.event.addListener(marker, 'mouseout', function (event) {
-    this.setOptions({
-      //strokeColor: '#ff0000',
-      //fillColor: getColour(dataPart.val)
-    });
-    infoWindow.close(map);
-  });
-}
-function drawInfoWindow(dataPart, map, cc){
-  //var featureData = {
-  //  name: dataPart.name
-  //}
-  var featureData = dataPart
-  var dataText = `
-    <div class="popupData">
-      <h2>${featureData.name}</h2>
-      <p>${featureData.longName}</p>
-      <p>UTC: ${featureData.datetime.toISOString()}</p>
-      <p>Started at: ${featureData.startTime}</p>
-    </div>
-  `
-  var infoWindow = new google.maps.InfoWindow({
-    content: dataText
-  });
-  var bounds = new google.maps.LatLngBounds();
-  cc.forEach(c=>bounds.extend(c))
-  infoWindow.setPosition(bounds.getCenter());
-  return infoWindow
+    var removeMarkers = function(data){
+      var markers = store.markers
+      data.forEach(d=>{
+        markers[d.vehicle_id].setMap(null)
+        markers[d.vehicle_id] = undefined
+        delete markers[d.vehicle_id]
+      })
+      // data.map(d=>d.remove)
+    }
+    var addMarkers = function(data){
+      data.forEach(d=>{
+        store.markers[d.vehicle_id] = markerHelper.drawMarker(d, map)
+      })
+    }
+    moveMarkers(groupedData.moveData)
+    removeMarkers(groupedData.removedData)
+    addMarkers(groupedData.addedData)
+  }
 }
 
