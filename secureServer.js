65d81c443ca7b9160a36faeb460574a9639164b4
Ben Lu <ben@ben-l.com>
2016-08-27 05:51:00 +1200

    Updated with redirector

--- secureServer.js
+++ secureServer.js
@@ -1,7 +1,20 @@
 var fs = require('fs')
+, http = require('http')
 , https = require('https')
 , app = require('./app')
  
+ var redir = http.createServer(function(req, res){
+  if (req.url.indexOf('.well-known') > -1) {
+    serve(req, res, finalhandler(req, res));
+    return;
+  }
+  res.writeHead(302, {
+    'Location': 'https://'+req.headers.host+req.url
+  });
+  res.end();
+});
+redir.listen(80);
+
 var options = {
   key: fs.readFileSync('/etc/letsencrypt/live/aklbuses.nz/privkey.pem')
 , cert: fs.readFileSync('/etc/letsencrypt/live/aklbuses.nz/fullchain.pem')
